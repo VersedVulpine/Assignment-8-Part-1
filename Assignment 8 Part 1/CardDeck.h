ab42eecd362b61cc939e8f483aff1e2714f7ba83
John Kim <JohnKim100@outlook.com>
2023-10-24 02:02:57 -0700

    Added Deck Card Class

---
+++ Assignment 8 Part 1/CardDeck.h
@@ -0,0 +1,17 @@
+#pragma once
+class CardDeck
+{
+
+
+private:
+	
+	int suit; //Card Suit (Spade, Clover, Heart, Diamond)
+	int value; // Card Value (1	to 13)
+
+
+public:
+	CardDeck() {} // Initializer
+	~CardDeck() {} //Destructor
+
+};
+
