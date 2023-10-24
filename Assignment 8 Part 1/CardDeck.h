4573beae259ee8d5ac1c7de8fbffcf81692ead24
John Kim <JohnKim100@outlook.com>
2023-10-24 02:26:46 -0700

    Added suits class in string form and a basic public class

--- Assignment 8 Part 1/CardDeck.h
+++ Assignment 8 Part 1/CardDeck.h
@@ -1,24 +1,36 @@
 #pragma once
 
 #include <iostream>
+
+#include<string> // for naming the card Suit. must find a better way of doing this...
+
+
 using namespace std;
 
 class CardDeck
 {
 
-
 private:
 	
-	int suit; //Card Suit (Spade, Clover, Heart, Diamond)
-	int value; // Card Value (1	to 13)
+	//int suit;	// old method of tracking suits
+	string suits[4] = { "Clover", "Diamond", "Heart", "Spade" }; //Card Suit (Spade, Clover, Heart, Diamond)
+	int value;	// Card Value (1 to 13)
 
 
 public:
 	CardDeck() {}	// Initializer
-	~CardDeck() {} //Destructor
+	~CardDeck() {}	// Destructor
 	
-	void setDeckSize(int newSize);
+	void setDeckSize(int newSize);	// The max Value of a Card in a suit
+	void getDeckSize();				// The highest value, times the suit amount (x * 4)
 
+	void setShuffleDeck();			// will get the size and shuffle the deck in random order
+	void getShuffleDeck();			//
+	
+	void getCard();					// will pull a card from a Deck, and remove it from the vector
+	void returnCard();				// adds back a value of a card to the deck, unless the card is already in the deck.
+
+	
 
 };
 
